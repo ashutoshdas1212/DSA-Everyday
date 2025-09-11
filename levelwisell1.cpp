86a116fefbf1d7027b6bd9d14040e2cb1cef4c8e
ashutoshdas1212 <ashutoshdas1212@gmail.com>
2025-09-11 20:23:58 +0530

    Auto DSA commit - 11-09-2025 20.23.58.70

---
+++ levelwisell1.cpp
@@ -0,0 +1,41 @@
+vector<Node<int> *> constructLinkedListForEachLevel(BinaryTreeNode<int> *root)
+{
+
+    vector< Node<int>*> v;
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    Node<int*> rootnode=new Node<int*> (root->data);
+    rootnode->next=NULL;
+
+    BinaryTreeNode<int *> blank=new BinaryTreeNode<int>(-1);
+    q.push(blank);
+    Node<int *>head=NULL;
+    Node<int *>tail=NULL;
+
+    while(!q.empty() && q.size()!=1)
+    {
+        BinaryTreeNode<int> *front=q.front();
+        q.pop();
+
+        if(front->data!=-1)
+        {
+            Node<int* > node=new Node<int>(front->data);
+            if(head==NULL)
+            {
+                head=node;
+                tail=node;
+                v.push(head);
+
+            }
+            else{
+                tail->next=node;
+                tail=node;
+            }
+        }
+
+        
+
+
+    }
+
+}
