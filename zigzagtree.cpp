c6c9a103497d00de882e5231c7f03e12dc2bf711
ashutoshdas1212 <ashutoshdas1212@gmail.com>
2025-09-16 20:24:00 +0530

    Auto DSA commit - 16-09-2025 20.23.59.63

--- zigzagtree.cpp
+++ zigzagtree.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 void zigZagOrder(BinaryTreeNode<int> *root)
 
 {
@@ -9,7 +10,7 @@ void zigZagOrder(BinaryTreeNode<int> *root)
     BinaryTreeNode<int> *blank = new BinaryTreeNode<int>(-1);
     q.push(blank);
 
-    while (!q.empty() && q.size() != 1)
+    while (!q.empty() )
     {
 
         BinaryTreeNode<int> *front = q.front();
@@ -19,14 +20,13 @@ void zigZagOrder(BinaryTreeNode<int> *root)
         if (front->data == -1)
         {
             cnt += 1;
-            if (!s.empty())
-            {
+           
                 while (!s.empty())
                 {
                     cout << s.top()->data << " ";
                     s.pop();
                 }
-            }
+            
             cout << endl;
             if (!q.empty())
             {
@@ -58,3 +58,59 @@ void zigZagOrder(BinaryTreeNode<int> *root)
 }
 
 
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+#include "solution.h"
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    zigZagOrder(root);
+}
